77f2186b9c112fc33dbc135da4119cf3cfc6f8d0
Mephbush <mghbwshmghbwsh9@gmail.com>
2025-11-09 06:03:37 +0300

    Update and rename MainActivity.java to MainActivity.kt

--- android/app/src/main/java/com/viscendstudio/app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.viscendstudio.app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/java/com/viscendstudio/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.viscendstudio.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
